dbe9554136f19322505d464785637abd133fc0b7
root <root@vs.mendrala.net>
2019-09-14 16:55:58 +0200

    d

--- addons.py
+++ addons.py
@@ -1,62 +1,16 @@
-
-_BEAM_ = ["service.subtitles.titulky.com","service.subtitles.serialzone.cz"]
-# list of addons that we are releasing
-
 __ADDONS__ = [
-    "plugin.video.barrandov.tv",
-    "plugin.video.befun.cz",
     "plugin.video.dmd-czech.aktualne",
     "plugin.video.dmd-czech.novaplus",
-    "plugin.audio.dmd-czech.play.cz",
-    "plugin.video.dmd-czech.playtvak",
     "plugin.video.dmd-czech.stream",
-    "plugin.video.gordon.ura.cz",
-    "plugin.video.hejbejse.tv",
-    "plugin.video.idnestv",
     "plugin.video.ivysilani",
-    "plugin.video.jaksetodela.cz",
-    "plugin.video.joj.sk",
-    "plugin.video.markiza.sk",
-    "plugin.video.mtr.sk",
-    "plugin.video.o2tvgo",
-    "plugin.video.online-files",
-    "plugin.video.pohadkar.cz",
     "plugin.video.primaplay",
-    "plugin.video.rtvs.sk",
-    "plugin.video.sosac.ph",
-    "plugin.video.sledujufilmy.cz",
-    "plugin.video.slowtv.cz",
-    "plugin.video.sport5",
-    "plugin.video.ta3.com",
-    "plugin.video.teevee.sk",
-    "plugin.video.tipsport.elh",
-    "plugin.video.tv.lux.sk",
-    "plugin.video.tv.sme.sk",
-    "plugin.video.tv.sosac.ph",
-    "plugin.video.videacesky.cz",
-    "plugin.video.zkouknito.cz",
-    "script.module.dmd-czech.common",
     "script.module.stream.resolver",
     "script.module.demjson",
-    "repository.xbmc.doplnky",
-    "repository.dmd-xbmc.googlecode.com",
     "repository.kodi-czsk",
-    "weather.dmd-xbmc.in-pocasi",
-    "plugin.video.dmd-czech.huste",
-    "metadata.bbaron",
     "plugin.video.stream-cinema",
-    "plugin.video.serialhd.cz",
-    "metadata.csfd.cz",
-    "plugin.image.rouming",
-    "metadata.serialzone.cz",
-    "plugin.video.mojevideo.sk",
-    "plugin.video.titulkomet.cz",
-    "weather.shmu.pocasie",
-    "skin.estuary.stream-cinema",
-    "plugin.video.sl",
     "plugin.video.mall.tv",
-    "plugin.video.bombuj.filmyserialy"
-    ] + _BEAM_
+    "plugin.video.dmd-czech.xtv"
+    ]
 
 import os
 import requests
