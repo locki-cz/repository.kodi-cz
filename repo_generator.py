76ab813a6dbb7f23071a3016a009e3777b59f510
root <root@vs.mendrala.net>
2019-09-14 20:52:08 +0200

    fsd

--- repo_generator.py
+++ repo_generator.py
@@ -169,4 +169,4 @@ if ( __name__ == "__main__" ):
 #            print 'New Name',zipfilenamefirstpart+version+zipfilenamelastpart
             shutil.move( os.path.join(os.path.join(os.getcwd(),zipsfolder),zipfilenamefirstpart+zipfilenamelastpart),os.path.join(os.path.join(os.getcwd(),zipsfolder),zipfilenamefirstpart+version+zipfilenamelastpart))
             #,zipfilenamefirstpart+version+zipfilenamelastpart);
-            #print 'Zipping ' + zipfilename + ' and moving to ' + zipfilenamefirstpart+version
+            print 'Zipping ' + zipfilename + ' and moving to ' + zipfilenamefirstpart+version
