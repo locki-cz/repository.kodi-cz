2505136961744e8a0e5816fe3fffb209c762c20b
root <root@vs.mendrala.net>
2019-09-15 20:14:39 +0200

    fsd

--- addons.py
+++ addons.py
@@ -10,7 +10,7 @@ for addon in os.listdir(os.curdir):
 # this function asks our repository and returns addons with different versions than in our local repo = candidates to be
 # released
 def find():
-    released_addons = requests.get('https://raw.githubusercontent.com/lama18/repository/master/repo/addons.xml').text
+    released_addons = open('repo/addons.xml', 'r').read()
     try:
         root = ET.XML( released_addons.encode('utf-8') )
     except: # initially there are no addons.xml
